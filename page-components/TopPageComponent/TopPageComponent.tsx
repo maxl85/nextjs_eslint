16a382d32134dcee3dc7e3b30bd6f5d607f8f150
maxl85 <maxl85@mail.ru>
2023-01-13 12:46:55 +0300

    14

---
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -0,0 +1,35 @@
+import { TopPageComponentProps } from './TopPageComponent.props';
+import styles from './TopPageComponent.module.scss';
+import cn from 'classnames';
+import { Advantages, HhData, Htag, P, Sort, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import { SortEnum } from '../../components/Sort/Sort.props';
+
+export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag='h1'>{page.title}</Htag>
+        {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+        <Sort sort={SortEnum.Rating} setSort={() => { }} />
+      </div>
+      <div>
+        {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+      </div>
+      <div className={styles.hhTitle}>
+        <Htag tag='h2'>Вакансии - {page.category}</Htag>
+        <Tag color='red' size='m'>hh.ru</Tag>
+      </div>
+      {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh} />}
+      {page.advantages && page.advantages.length > 0 &&
+        <>
+          <Htag tag='h2'>Преимущества</Htag>
+          <Advantages advantages={page.advantages} />
+        </>
+      }
+      {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
+      <Htag tag='h2'>Получаемые навыки</Htag>
+      {page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
+    </div>
+  );
+};
